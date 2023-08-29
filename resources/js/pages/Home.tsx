848d93a3b63f5f41f693ab20dd20baa4a1aaf4cb
riveur <riveur.cpy@gmail.com>
2023-08-29 09:03:19 +0200

    feat: setup frontend

---
+++ resources/js/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { Head } from '@inertiajs/react';
+
+export default function Welcome() {
+    return (
+        <>
+            <Head title="Home" />
+            <h1>Home page</h1>
+        </>
+    );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.tsx',
+    ],
+
+    theme: {
+        extend: {},
+    },
+    plugins: []
+};
